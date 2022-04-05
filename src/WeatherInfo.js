5a3f3db99c973586021c00aab53d0fe483dbcb56
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-04-05 11:28:02 +0100

    Readded temperature conversion component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,37 +1,40 @@
 import React from "react";
 import DateTime from "./DateTime";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
-    return (
-        <div className="WeatherInfo">
-        <h1 className="Weather__title">Current weather: <strong>{props.data.city}</strong></h1>
-            <ul className="Weather__primary-list">
-                <li>
-                    <DateTime date={props.data.date} />
-                </li>
-                <li className="text-capitalize">
-                    {props.data.description}
-                </li>
-            </ul>
-            <div className="row">
-                <div className="col-md-6">
-                    <div className="float-left">
-                        <WeatherIcon code={props.data.icon} alt={props.data.description} />
-                        {props.data.temperature}<span className="Weather__secondary-list--units">C</span>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <ul className="Weather__secondary-list">
-                        <li>
-                            Humidity: {props.data.humidity}<span className="Weather__secondary-list--units">%</span>
-                        </li>
-                        <li>
-                            Windspeed: {props.data.windspeed}<span className="Weather__secondary-list--units">km/ph</span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="WeatherInfo">
+      <h1 className="Weather__title">
+        Current weather: <strong>{props.data.city}</strong>
+      </h1>
+      <ul className="Weather__primary-list">
+        <li>
+          <DateTime date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="float-left">
+            <WeatherIcon code={props.data.icon} alt={props.data.description} />
+            <WeatherTemperature celsius={props.data.temperature} />
+          </div>
         </div>
-    );
+        <div className="col-md-6">
+          <ul className="Weather__secondary-list">
+            <li>
+              Humidity: {props.data.humidity}
+              <span className="Weather__secondary-list--units">%</span>
+            </li>
+            <li>
+              Windspeed: {props.data.windspeed}
+              <span className="Weather__secondary-list--units">km/ph</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
